f687481bb40649de592ab898fc5c165e49669e00
abstract_field <55926224+liyayunusova@users.noreply.github.com>
2024-10-28 14:19:34 +0300

    Create qwen_helper.py

---
+++ team-06/reports/telegram_bot_summarizer_with_qwen2.5/qwen_helper.py
@@ -0,0 +1,17 @@
+"""
+Р—РґРµСЃСЊ РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ РјРµС‚РѕРґС‹ СЂР°Р±РѕС‚С‹ СЃ РјРѕРґРµР»СЊСЋ Qwen2.5
+"""
+
+import logging
+
+logger = logging.getLogger("bot")
+logger.setLevel("DEBUG")
+
+
+class QwenHelper:
+    def __init__(self):
+        logging.info(f"Initializing Qwen helper.")
+        pass
+
+    def get_response(self, text):
+        pass
