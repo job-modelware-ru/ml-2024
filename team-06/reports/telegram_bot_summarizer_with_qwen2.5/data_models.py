e9ebb8ef5192ba1fc7abb29c5eb996d91bcee226
abstract_field <55926224+liyayunusova@users.noreply.github.com>
2024-10-28 14:17:05 +0300

    Create data_models.py

---
+++ team-06/reports/telegram_bot_summarizer_with_qwen2.5/data_models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class Chat(BaseModel):
+    id: int
+
+
+class Message(BaseModel):
+    text: str
+    chat: Chat
+
+
+class Update(BaseModel):
+    update_id: int
+    message: Message
